4c803d1174d5f08b66b1cdd3b59aa7ace73875e6
Rohan Thomas <rohangeorgethomas@gmail.com>
2018-06-07 14:32:12 +0800

    upload portion of the task

---
+++ task3.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<title>Task 3</title>
+</head>
+<body>
+	<form action="upload.php" method="post" enctype="multipart/form-data">
+		Select file to upload:
+		<input type="file" name="uploadfile">
+		<input type="submit" name="upload" value="Upload">
+	</form>
+
+</body>
+</html>
---
+++ upload.php
@@ -0,0 +1,3 @@
+<?php
+	$folder="uploads/";
+	$file_path=$folder.basename($_FILES["uploadfile"]["name"])
