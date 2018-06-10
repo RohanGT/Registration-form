302f468497307fbe2bae5fe44ef277ff81ceecbb
Rohan Thomas <rohangeorgethomas@gmail.com>
2018-06-10 16:38:11 +0800

    Updated upload file

--- task3.php
+++ task3.php
@@ -5,11 +5,74 @@
 	<title>Task 3</title>
 </head>
 <body>
-	<form action="upload.php" method="post" enctype="multipart/form-data">
+
+	<?php
+		$folder="uploads/";
+		$fname=$_FILES["uploadfile"]["name"];
+		$fsize=$_FILES["uploadfile"]["size"];
+		$ftype=$_FILES["uploadfile"]["type"];
+		$ext=strtolower(substr($fname, strpos($fname, '.')+1));
+		$file=$folder.basename($fname);
+
+		$flag=1;
+
+		if(isset($fname))
+		{
+			if (!empty($fname)) {
+				if(!file_exists($file)){
+					if($ftype=='application/pdf'&& $ext=='pdf')
+					{
+						echo '';
+					}
+					else{
+						echo "File must be pdf <br>";
+						$flag=0;
+					}
+
+					if($fsize<=5120000)
+					{
+						echo '';
+					}
+					else{
+						echo "File must be under 5mb<br>";
+						$flag=0;
+					}
+				}
+				else{
+					echo "This file already exists";
+					$flag=0;
+				}
+			}
+			else
+			{
+				echo "Please choose a file.";
+				$flag=0;
+			}
+
+		if($flag){
+			if(move_uploaded_file($_FILES["uploadfile"]["tmp_name"], $file)){
+				echo "File was uploaded succesfully";
+			}
+			else{
+				echo "Sorry there was an error in uploading. Please try again.";
+			}
+		}	
+		else
+		{
+			echo '';
+		}
+		}
+
+		
+	?>
+
+	<form action="task3.php" method="POST" enctype="multipart/form-data">
 		Select file to upload:
 		<input type="file" name="uploadfile">
+		<br>
 		<input type="submit" name="upload" value="Upload">
 	</form>
 
 </body>
 </html>
+
