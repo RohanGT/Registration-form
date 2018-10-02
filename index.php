472a826997f6381f83fa6cc233611e772b8593b6
anshitabaid <anshita_b@yahoo.co.in>
2018-10-02 14:00:15 +0530

    restructed wd

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 <head>
     <title>Registration</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 
